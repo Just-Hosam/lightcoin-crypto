3d5f6d24abed53f24f49c1de47a066cbc2fecefd
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-14 01:56:24 +0000

    added stretch

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ class Account {
   }
 
   get balance() {
-    // Calculate the balance using the transaction objects.
+    let sum = 0;
+    for (const elem of this.transactions) {
+      sum += elem.value;
+    }
+    return sum;
   }
 
   addTransaction(transaction) {
@@ -23,18 +27,26 @@ class Transaction {
   }
 
   commit() {
-    // Keep track of the time of the transaction
-    this.time = new Date();
-    // Add the transaction to the account
-    this.account.addTransaction(this);
+    if (this.isAllowed === true) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    } else {
+      console.log(this.isAllowed);
+    }
   }
-
 }
 
 class Deposit extends Transaction {
 
   get value() {
     return this.amount;
+  }
+
+  get isAllowed() {
+    if (this.amount > 10000) {
+      return 'You are depositing over $10,000. Permission denied.';
+    }
+    return true;
   }
 
 }
@@ -45,6 +57,13 @@ class Withdrawal extends Transaction {
     return -(this.amount);
   }
 
+  get isAllowed() {
+    if (this.amount > this.account.balance) {
+      return 'Insufficient funds!';
+    }
+    return true;
+  }
+
 }
 
 
@@ -52,13 +71,15 @@ class Withdrawal extends Transaction {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 const myAccount = new Account('billybob');
+console.log(myAccount);
 
 console.log('Starting Balance:', myAccount.balance);
 
 const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-
 const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
+const t3 = new Withdrawal(80, myAccount);
+t3.commit();
 
 console.log('Ending Balance:', myAccount.balance);
